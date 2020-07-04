dc7114647ede58a7769af0313a5c8ee0a1495ba0
Jeremy May <may.jeremy.r@gmail.com>
2020-07-03 19:36:44 -0700

    display basic transaction data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const infuraUrl = 'https://mainnet.infura.io/v3/835d112a6b6043b2becf7ee5f739cac3';
 var web3 = new Web3(infuraUrl);
+var BN = web3.utils.BN;
 
 const datasources = {
   live: 'LIVE',
@@ -41,10 +42,53 @@ const DATASOURCE = datasources.live;
       { name: "Gas Limit", val: block.gasLimit },
       { name: "Extra Data", val: block.extraData }
     ];
+    const tableInfo = [
+      { header: "Txn Hash", value: trans => trans.hash },
+      // { header: "Block", value: trans => trans.blockNumber },
+      // { header: "Age", value: "caclulated" },
+      { header: "From", value: trans => trans.from },
+      { header: "To", value: trans => trans.to },
+      { header: "Value", value: trans => web3.utils.fromWei(trans.value, "ether") },
+      {
+        header: "[Txn Fee]",
+        value: trans => {
+          let fee = new BN(trans.gasPrice).mul(new BN(trans.gas));
+          return web3.utils.fromWei(fee, "ether");
+        }
+      },
+    ];
+
     let divLatestTrans = $('#divLatestTrans');
     divLatestTrans.append(`<h5>Block #${block.number}</h5>`);
 
+    // block header
     blockInfo.forEach(item => divLatestTrans.append(`<p>${item.name}: ${item.val}</p>`));
+
+    // transactions
+    let elements = `
+      <table class="table">
+        <thead>          
+    `;
+    tableInfo.forEach(item => elements +=
+      `   <th scope="col">${item.header}</th>`);
+    elements += `
+        </thead>
+        <tbody>
+          `;
+    block.transactions.forEach(trans => {
+      elements +=
+        `   <tr>`;
+      tableInfo.forEach(cell => elements +=
+        `     <td>${cell.value(trans)}</td>
+        `);
+      elements +=
+        `    </tr>`;
+
+    });
+    elements += `
+        </tbody>
+      </table>`;
+    divLatestTrans.append(elements);
   }
 
   async function getLatestTransactiions() {
